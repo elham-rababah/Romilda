9f65e1e64aecb78bafbc1ff7b855145bc04a0f7d
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-07-17 18:27:54 +0300

    frined

---
+++ client/app/frined/frined.js
@@ -0,0 +1,13 @@
+angular.module('m.frined', [])
+
+.controller('frinedController', function ($scope) {
+	$scope.frined=[];
+	$scope.sendToFrined=function(){
+
+	}
+
+	$scope.remove=function(){
+
+	}
+
+});
