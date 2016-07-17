1b8abc97ac75a10cf9d0b45065154382b6ad6367
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-07-17 14:46:36 +0300

    auth & indexfile

---
+++ client/app/auth/auth.js
@@ -0,0 +1,35 @@
+angular.module('memorize.auth', [])
+
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+
+
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.memorize', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+
+
+
+
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.memorize', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+});
